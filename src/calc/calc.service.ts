918240f76fdf09660962c445fa267f30f51f034b
yash <yashmehta766@gmail.com>
2024-04-06 18:14:09 +0530

    implemented error handeling

--- src/calc/calc.service.ts
+++ src/calc/calc.service.ts
@@ -1,34 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CalcDto } from './calc.dto';
 
 @Injectable()
 export class CalcService {
   calculateExpression(calcBody: CalcDto) {
-    const expressionArray = calcBody.expression.match(/\d+|\+|\-|\*|\//g);
-    const operate = (num1: number, operator: string, num2: number): number => {
-      switch (operator) {
-        case '+':
-          return num1 + num2;
-        case '-':
-          return num1 - num2;
-        case '*':
-          return num1 * num2;
-        case '/':
-          if (num2 === 0) {
-            throw new Error('Division by zero');
-          }
-          return num1 / num2;
-        default:
-          throw new Error(`Invalid operator ${operator}`);
+    try {
+      const expressionArray = calcBody.expression.match(/\d+|\+|\-|\*|\//g);
+      const regex = /^(\d+(\.\d+)?)([-+*/](\d+(\.\d+)?))*$/;
+      const isValid = regex.test(calcBody.expression);
+      // Check if the calcBody is valid
+      if (!expressionArray || expressionArray.length % 2 === 0 || !isValid) {
+        throw new Error('Invalid calcBody provided');
       }
-    };
-    let result = parseFloat(expressionArray[0]);
-    for (let i = 1; i < expressionArray.length; i += 2) {
-      const operator = expressionArray[i];
-      const operand = parseFloat(expressionArray[i + 1]);
-      result = operate(result, operator, operand);
-    }
+      const operate = (
+        num1: number,
+        operator: string,
+        num2: number,
+      ): number => {
+        switch (operator) {
+          case '+':
+            return num1 + num2;
+          case '-':
+            return num1 - num2;
+          case '*':
+            return num1 * num2;
+          case '/':
+            if (num2 === 0) {
+              throw new Error('Division by zero');
+            }
+            return num1 / num2;
+          default:
+            throw new Error(`Invalid operator ${operator}`);
+        }
+      };
+      let result = parseFloat(expressionArray[0]);
+      for (let i = 1; i < expressionArray.length; i += 2) {
+        const operator = expressionArray[i];
+        const operand = parseFloat(expressionArray[i + 1]);
+        result = operate(result, operator, operand);
+      }
 
-    return result;
+      return result;
+    } catch (error) {
+      throw new HttpException(
+        {
+          statusCode: HttpStatus.BAD_REQUEST,
+          message: 'Invalid expression provided',
+          error: 'Bad Request',
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 }
